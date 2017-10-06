3cae4d62609691f4313c73968e8e369afee4cbb2
Luis Felipe Ferreira <luislipe08@gmail.com>
2017-10-06 15:21:27 -0300

    AV2
    TagSelector

---
+++ Assets/Editor/TagEditoPopup.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomPropertyDrawer(typeof(TagSelector))]
+
+
+
+public class TagEditorPopup : PropertyDrawer
+{
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.propertyType == SerializedPropertyType.String)
+        { 
+            EditorGUI.BeginProperty(position, label, property);
+            var attrib = this.attribute as TagSelector;
+            if (attrib.Tag)
+            {
+                property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+            }
+            else
+            {
+                List<string> tagList = new List<string>();
+                string propertyString = property.stringValue;
+                int index = -1;
+                tagList.Add("<Tag Não Selecionada>");
+                tagList.AddRange(UnityEditorInternal.InternalEditorUtility.tags);
+                if (propertyString == "")
+                {
+                    index = 0;
+                }
+                  else
+                {
+                    for (int i = 1; i < tagList.Count; i++)
+                    {
+                        if (tagList[i] == propertyString)
+                        {
+                            index = i;
+                            break;
+                        }
+                    }
+                }
+                index = EditorGUI.Popup(position, label.text, index, tagList.ToArray());
+                if (index == 0)
+                {
+                    property.stringValue = "";
+                }
+                else if (index >= 1)
+                {
+                    property.stringValue = tagList[index];
+                }
+                else
+                {
+                    property.stringValue = "";
+                }
+            }
+        }
+    }
+}
+
+
